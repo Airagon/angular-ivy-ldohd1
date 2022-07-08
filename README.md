3dd4781f7f43651b974686ed37d57fc7e6a7453a
Airagon <aldo7liverpool@gmail.com>
2022-07-08 11:39:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ldohd1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ldohd1)
---
+++ src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'vikelaia-qr-scanner';
+  qrResultString: string;
+  shownButton = Buttons.SCAN;
+
+  scannerEnabled = false;
+
+  encryptText = 'lalala';
+  decPassword = 'aasda';
+  conversionDecryptOutput!: string;
+
+  scannerHasResult = false;
+
+  correctResult = false;
+
+  ngOnInit() {
+    // action.toggleCamera()
+  }
+
+  clearResult(): void {
+    this.qrResultString = null;
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+  }
+
+  enableScanner() {
+    this.scannerEnabled = true;
+    this.shownButton = Buttons.STOP;
+  }
+
+  disableScanner() {
+    this.scannerEnabled = false;
+    this.shownButton = Buttons.SCAN;
+  }
+
+  scanSuccessHandler($event) {
+    this.conversionDecryptOutput = CryptoJS.AES.decrypt(
+      $event.trim(),
+      this.decPassword.trim()
+    ).toString(CryptoJS.enc.Utf8);
+
+    this.scannerHasResult = true;
+
+    if (this.conversionDecryptOutput === this.encryptText) {
+      this.correctResult = true;
+    } else this.correctResult = false;
+
+    this.shownButton = Buttons.CONFIRM;
+  }
+
+  reScan() {
+    this.scannerHasResult = false;
+    this.shownButton = Buttons.STOP;
+  }
+}
+
+enum Buttons {
+  SCAN,
+  STOP,
+  CONFIRM,
+}
